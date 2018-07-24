71b8704290d48c77ea25775d7bf1d4c4f108b062
pallav <pallavraj40@gmail.com>
2018-07-24 15:46:00 +0530

    added functionality

--- OMDB-API/app.js
+++ OMDB-API/app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const request = require('request');
 app.set("view engine" , "ejs");
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/",function (req , res) {
+    res.render("search");
+})
 
 app.get('/results', function (req , res) {
+    var q = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + q + "&apikey=7848dea3"
 
-    request("http://www.omdbapi.com/?s=star&apikey=7848dea3" , function (error , response , body) {
+    request(url ,  function (error , response , body) {
         if(!error && response.statusCode==200){
             var data = JSON.parse(body);
             res.render("results" , {data : data});
